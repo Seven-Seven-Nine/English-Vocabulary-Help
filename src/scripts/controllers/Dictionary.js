6dc9df779cdf6272d0e607ebb8b7e18a11192e95
Seven-Seven-Nine <danila.umbetaliev.05@mail.ru>
2024-10-27 17:07:14 +0500

    Сношение карточек

--- src/scripts/controllers/Dictionary.js
+++ src/scripts/controllers/Dictionary.js
@@ -2,6 +2,7 @@ import Controllers from "./Controller.js";
 import Sound from "../Sound.js";
 import MainMenu from "./MainMenu.js";
 import Words from "../Words.js";
+import FileLoader from "../FileLoader.js";
 
 /**
  * @class
@@ -59,19 +60,25 @@ export default class Dictionary extends Controllers {
     }
 
     /**
-     * Добавление слов в вёрстку.
+     * Добавление словарных карточек в DOM.
      * @private
      */
     addWordsInDom() {
         this.getWords();
+
+        let btnArr = [];
+        let idForBtn = -1;
+
         let mainPanel = document.getElementById('dictionary-main-panel');
+        
         let keysObject = Object.keys(this.words);
         let lengthObject = keysObject.length;
+
         for (let index = 0; index < lengthObject; index++) {
             // this.words[keysObject[index]]
             let lengthWords = Object.keys(this.words[keysObject[index]]).length;
             for (let indexWords = 0; indexWords < lengthWords; indexWords++) {
-                console.info(this.words[keysObject[index]][indexWords]);
+                idForBtn++;
                 mainPanel.innerHTML += `
                     <div class="dictionary-card">
                         <div class="dictionary-card-title">
@@ -80,12 +87,43 @@ export default class Dictionary extends Controllers {
                         <div class="dictionary-card-translate">
                             <p>${this.words[keysObject[index]][indexWords][1]}</p>
                         </div>
-                        <div>
-                            <div>Иконка!</div>
+                        <div class="dictionary-card-icon">
+                            <div id="ico-add-${idForBtn}">
+                                ${FileLoader.loadSVG('ico-add.svg')}
+                            </div>
                         </div>
                     </div>
                 `;
+                let btn = `ico-add-${idForBtn}`;
+                btnArr.push(btn);
             }
+        }
+
+        // console.info(`ID кнопок: ${btnArr}`);
+
+        // Добавление событий для кнопок.
+        for (let index = 0; index < btnArr.length; index++) {
+            /** 
+             * @type {HTMLElement} 
+             * */ 
+            // console.info(`Добавление события для ${btnArr[index]}.`);
+            let btnAdd = document.getElementById(btnArr[index]);
+            btnAdd.onclick = () => this.btnAddWord(btnArr[index]);
+            // console.info(`Событие добавлено для ${btnArr[index]}!`);
+        }
+    }
+    
+    /**
+     * Кнопка добавления слова в словарь.
+     * @private
+     * @param {string} idWord 
+    */
+   btnAddWord(idWord) {
+        if (typeof idWord !== 'string') {
+            throw new TypeError(`Неправильный тип параметра 'idWord' - ${typeof idWord}`);
+        } else {
+            console.info(`Событие кнопки ${idWord}...`);
+            Sound.soundNewFrame();
         }
     }
 }
